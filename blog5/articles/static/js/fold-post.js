d1aab3b55b4a7811dbde5ac18f6b95e901c3113b
cuttenEDU <z.zhmakin@gmail.com>
2019-05-23 20:21:02 +0300

    started lab8

--- blog5/articles/static/js/fold-post.js
+++ blog5/articles/static/js/fold-post.js
@@ -1,7 +1,7 @@
 var foldBtns = document.getElementsByClassName("fold-button");
 for (var i = 0; i < foldBtns.length; i++) {
     foldBtns[i].addEventListener("click", function (event) {
-        if (event.target.parentElement.parentElement.className == "one-post folded") {
+        if (event.target.parentElement.parentElement.className === "one-post folded") {
             event.target.innerHTML = "Cвернуть";
             event.target.parentElement.parentElement.className = "one-post"
         } else {
